e1b60206b4799d8ee03d82d2564d6c9a9c294252
ng1n3 <code.with.muyiwa@gmail.com>
2024-12-28 01:50:02 +0100

    create, read, read single feature

--- controllers/todoController.ts
+++ controllers/todoController.ts
@@ -1,2 +1,86 @@
-import { todos } from "../db.ts";
-import { ObjectId } from "mongodb";
+import { ObjectId } from 'mongodb';
+import { todos } from '../db.ts';
+
+async function addTodo(req: Request): Promise<Response> {
+  try {
+    const body = await req.json();
+    const result = await todos.insertOne(body);
+    return new Response(JSON.stringify({ id: result.insertedId }), {
+      status: 201,
+      headers: { 'Content-Type': 'application/json' },
+    });
+  } catch (error: any) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      status: 400,
+      headers: { 'Content-Type': 'application/json' },
+    });
+  }
+}
+
+async function getTodos(): Promise<Response> {
+  try {
+    const allTodos = await todos.find().toArray();
+    return new Response(JSON.stringify(allTodos), {
+      headers: { 'Content-Type': 'application/json' },
+    });
+  } catch (error: any) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      status: 500,
+      headers: { 'Content-Type': 'application/json' },
+    });
+  }
+}
+
+async function getTodo(id: string): Promise<Response> {
+  try {
+    const todo = await todos.findOne({ _id: new ObjectId(id) });
+    if (!todo) {
+      return new Response(JSON.stringify({ error: 'Todo not found' }), {
+        status: 404,
+        headers: { 'Content-Tyyype': 'application/json' },
+      });
+    }
+    return new Response(JSON.stringify(todo), {
+      headers: { 'Content-Type': 'application/json' },
+    });
+  } catch (error: any) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      status: 500,
+      headers: { 'Content-Type': 'application/json' },
+    });
+  }
+}
+
+async function updateTodo(id: string, req: Request): Promise<Response> {
+  try {
+    const body = await req.json();
+    // const todo = await todos.findOne({ _id: new ObjectId(id) });
+    // if (!todo) {
+    //   return new Response(JSON.stringify({ error: 'Todo not found' }), {
+    //     status: 404,
+    //     headers: { 'Content-Tyyype': 'application/json' },
+    //   });
+    // }
+    const updateTodo = await todos.updateOne(
+      { _id: new ObjectId(id) },
+      { $set: body }
+    );
+    if (updateTodo.matchedCount === 0) {
+      return new Response(JSON.stringify({ error: 'Todo not found' }), {
+        status: 404,
+        headers: { 'Content-Tyyype': 'application/json' },
+      });
+    }
+
+    return new Response(JSON.stringify(updateTodo), {
+      headers: { 'Content-Type': 'application/json' },
+    });
+  } catch (error: any) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      status: 400,
+      headers: { 'Content-Type': 'application/json' },
+    });
+  }
+}
+
+export { addTodo, getTodo, getTodos, updateTodo };
--- server.ts
+++ server.ts
@@ -1,3 +1,5 @@
+import { addTodo, getTodo, getTodos, updateTodo } from './controllers/todoController.ts';
+
 const PORT = 5000;
 
 async function handler(req: Request): Promise<Response> {
@@ -6,15 +8,21 @@ async function handler(req: Request): Promise<Response> {
 
   if (req.method === 'GET' && path === '/') {
     return new Response('Hello world');
-  } else if (req.method == 'POST' && path === '/api/todos') {
-  } else if (req.method === 'GET' && path === '/api/todo') {
+  } else if (req.method == 'POST' && path === '/api/todo') {
+    return await addTodo(req);
+  } else if (req.method === 'GET' && path === '/api/todos') {
+    return await getTodos();
   } else if (req.method === 'GET' && path === '/api/todos/incomplete/count') {
   } else if (req.method === 'GET' && path.startsWith('/api/todos')) {
+    const id = path.split('/')[3]
+    return await getTodo(id)
   } else if (req.method === 'PUT' && path.startsWith('api/todos')) {
+    const id = path.split('/')[3]
+    return await updateTodo(id, req)
   } else if (req.method === 'DELETE' && path.startsWith('/api/todos')) {
   }
   return new Response('Not found', { status: 404 });
 }
 
-console.log(`HTTP server is runing on http://localhost$${PORT}`);
+console.log(`HTTP server is runing on http://localhost${PORT}`);
 Deno.serve({ port: PORT }, handler);
